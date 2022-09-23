7c599ce829843d5d1d680f4b6b31f9e01e635956
Tony Aguilar <onixag@gmail.com>
2022-09-23 20:10:12 +0200

    Remove unnecessary apt install

--- setup/scripts/debian-post-install.sh
+++ setup/scripts/debian-post-install.sh
@@ -130,7 +130,6 @@ log_highlight - ----------------------------------------------------------------
 log_highlight -
 log_highlight - [$APP_NAME] Installing specific package: brave-browser
 log_highlight -
-RUNCMD "sudo apt install apt-transport-https curl"
 RUNCMD "sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"
 RUNCMD "echo 'deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main'|sudo tee /etc/apt/sources.list.d/brave-browser-release.list"
 RUNCMD "sudo apt update"
