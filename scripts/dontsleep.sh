d1cd71bcb4e77bd5d771dbecbb2daa9a08890bc2
Tony Aguilar <onixag@gmail.com>
2022-09-28 21:29:22 +0200

    Add dontsleep script

---
+++ scripts/dontsleep.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+DEFAULT_DELAY=1
+DEFAULT_DISTANCE=100
+
+DELAY=${1:-$DEFAULT_DELAY}
+DISTANCE=${2:-$DEFAULT_DISTANCE}
+
+echo -
+echo - Keeping computer awake...
+echo - Delay: $DELAY
+echo - Distance: $DISTANCE
+echo -
+echo - Press CTRL+C to stop
+echo -
+
+function move_cycle {
+  for iteratin in {1..4}
+  do
+    echo -en "  → → → → \r"
+    xdotool mousemove_relative $DISTANCE 0
+    sleep $DELAY
+    echo -en "  ↓ ↓ ↓ ↓ \r"
+    xdotool mousemove_relative 0 $DISTANCE
+    sleep $DELAY
+    echo -en "  ← ← ← ← \r"
+    xdotool mousemove_relative -- -${DISTANCE} 0
+    sleep $DELAY
+    echo -en "  ↑ ↑ ↑ ↑ \r"
+    xdotool mousemove_relative -- 0 -${DISTANCE}
+    sleep $DELAY
+  done
+}
+
+while true
+do
+  move_cycle
+done
