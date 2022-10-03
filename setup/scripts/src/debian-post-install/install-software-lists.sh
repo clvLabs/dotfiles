e3aa18373425cda95eaa8a8b445c25276aa90bfe
Tony Aguilar <onixag@gmail.com>
2022-10-03 20:36:05 +0200

    Get rid of snaps

--- setup/scripts/src/debian-post-install/install-software-lists.sh
+++ setup/scripts/src/debian-post-install/install-software-lists.sh
@@ -7,20 +7,6 @@ RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt -y install $APT_INSTALL"
 
 log_highlight - ----------------------------------------------------------------------------
 log_highlight -
-log_highlight - [$APP_NAME] Installing package list for: snap
-log_highlight -
-SNAP_INSTALL=$(echo $(sed "s/#.*$//g" $SETUP_RESOURCES_DIR/requirements/snap-install.txt | cat))
-RUNCMD "sudo snap install $SNAP_INSTALL"
-
-log_highlight - ----------------------------------------------------------------------------
-log_highlight -
-log_highlight - [$APP_NAME] Installing package list for: snap --classic
-log_highlight -
-SNAP_INSTALL=$(echo $(sed "s/#.*$//g" $SETUP_RESOURCES_DIR/requirements/snap-classic-install.txt | cat))
-RUNCMD "sudo snap install $SNAP_INSTALL --classic"
-
-log_highlight - ----------------------------------------------------------------------------
-log_highlight -
 log_highlight - [$APP_NAME] Installing package list for: pip
 log_highlight -
 PIP_INSTALL=$(echo $(sed "s/#.*$//g" $SETUP_RESOURCES_DIR/requirements/pip-install.txt | cat))
