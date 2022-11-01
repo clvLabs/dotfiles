903a04c430a933ce801c8034b509982fca5eab96
Tony Aguilar <onixag@gmail.com>
2022-11-01 13:56:48 +0100

    Fix gitui setup

--- setup/scripts/src/debian-post-install/.specific-software.d/005-gitui.sh
+++ setup/scripts/src/debian-post-install/.specific-software.d/005-gitui.sh
@@ -6,10 +6,10 @@ SOURCE_PATH="https://github.com/extrawurst/gitui/releases/download/v0.21.0"
 SOURCE_FILE="gitui-linux-musl.tar.gz"
 
 DEST_PATH="${HOME}/apps/gitui"
+RUNCMD "mkdir -p ${DEST_PATH}"
 
 TEMP_FOLDER=$(mktemp -d)
 RUNCMD "wget -O ${TEMP_FOLDER}/${SOURCE_FILE} ${SOURCE_PATH}/${SOURCE_FILE}"
-RUNCMD "mkdir -p ${DEST_PATH}"
-RUNCMD "unzip ${TEMP_FOLDER}/${SOURCE_FILE} ${DEST_PATH}"
+RUNCMD "tar xvzf ${TEMP_FOLDER}/${SOURCE_FILE} -C ${DEST_PATH}"
 RUNCMD "ln -s ${DEST_PATH}/gitui ${HOME}/bin/gitui"
 RUNCMD "rm -rf ${TEMP_FOLDER}"
