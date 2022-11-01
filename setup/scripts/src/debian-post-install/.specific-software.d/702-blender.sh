7d34abeba2bde4e31dc3f3b0846b8985cb9e5fd9
Tony Aguilar <onixag@gmail.com>
2022-11-01 16:43:39 +0100

    Fix blender install

--- setup/scripts/src/debian-post-install/.specific-software.d/702-blender.sh
+++ setup/scripts/src/debian-post-install/.specific-software.d/702-blender.sh
@@ -8,7 +8,7 @@ _PATCH=0
 
 PACKAGE="blender-${_MAJOR}.${_MINOR}.${_PATCH}-linux-x64"
 SOURCE_PATH="https://download.blender.org/release/Blender${_MAJOR}.${_MINOR}"
-XZ_FILE="${PACKAGE}.tar.xz"
+SOURCE_FILE="${PACKAGE}.tar.xz"
 TAR_FILE="${PACKAGE}.tar"
 
 DEST_PATH="${HOME}/apps/blender"
@@ -16,8 +16,8 @@ RUNCMD "mkdir -p ${DEST_PATH}"
 
 TEMP_FOLDER=$(mktemp -d -t ${APP_NAME}-blender-XXXXXX)
 
-RUNCMD "wget --no-verbose -O ${TEMP_FOLDER}/${XZ_FILE} ${SOURCE_PATH}/${XZ_FILE}"
-RUNCMD "cd ${TEMP_FOLDER}; xz -dv ${XZ_FILE}"
-RUNCMD "tar xvf ${TAR_FILE} -C ${DEST_PATH}"
+RUNCMD "wget --no-verbose -O ${TEMP_FOLDER}/${SOURCE_FILE} ${SOURCE_PATH}/${SOURCE_FILE}"
+RUNCMD "cd ${TEMP_FOLDER}; xz -dv ${SOURCE_FILE}"
+RUNCMD "tar xvf ${TEMP_FOLDER}/${TAR_FILE} -C ${DEST_PATH}"
 RUNCMD "ln -sf ${DEST_PATH}/${PACKAGE}/blender ${HOME}/bin/blender"
 RUNCMD "rm -rf ${TEMP_FOLDER}"
