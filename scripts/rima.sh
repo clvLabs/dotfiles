2e469dadc6818a777a8de271aaef4306d302f931
Tony Aguilar <onixag@gmail.com>
2022-11-01 15:48:13 +0100

    Separate output by lines, remove wordcount

--- scripts/rima.sh
+++ scripts/rima.sh
@@ -3,7 +3,8 @@ WORDS=$(grep -r --color=always ${1}$ /usr/share/dict/spanish | tr '\n' ' ')
 RAWWORDS=$(grep -r ${1}$ /usr/share/dict/spanish | tr '\n' ' ')
 WORDCOUNT=$(grep -r --color=always ${1}$ /usr/share/dict/spanish | wc -w)
 
-echo "$WORDS\n\nTotal ${1}: $WORDCOUNT"
+echo "$WORDS" | sed "s/ /\n/g"
+# echo "$WORDS\n\nTotal ${1}: $WORDCOUNT"
 
 if [ "$2" = --defs ]; then
     echo ""
