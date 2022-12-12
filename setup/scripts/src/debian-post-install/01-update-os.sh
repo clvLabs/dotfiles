e3643729a490b112631bfe3ca25706b9bd8fe6ea
Tony Aguilar <onixag@gmail.com>
2022-12-12 18:45:49 +0100

    Add NVidia drivers

--- setup/scripts/src/debian-post-install/01-update-os.sh
+++ setup/scripts/src/debian-post-install/01-update-os.sh
@@ -1,5 +1,12 @@
 log_highlight - ----------------------------------------------------------------------------
 log_highlight -
+log_highlight - [${APP_NAME}] Enabling "contrib" and "non-free" package repositories
+log_highlight -
+RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt-add-repository contrib"
+RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt-add-repository non-free"
+
+log_highlight - ----------------------------------------------------------------------------
+log_highlight -
 log_highlight - [${APP_NAME}] Updating apt package indexes
 log_highlight -
 RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt-get -y update"
