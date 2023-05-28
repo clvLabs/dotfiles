805a81dd04408b1cd7d0ed60ab1d2c6d3fa847e9
Tony Aguilar <onixag@gmail.com>
2023-05-28 12:03:41 +0200

    Add daily-apt-update script

---
+++ scripts/daily-apt-update.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+CURR_DATE=$(date +%Y%m%d)
+LAST_UPDATE_FILE="${HOME}/.config/daily-apt-update-timestamp.txt"
+LAST_UPDATE=$(cat ${LAST_UPDATE_FILE} 2>&1 > /dev/null)
+
+echo "Daily apt update"
+
+if [ "${LAST_UPDATE}" = "${CURR_DATE}" ]
+then
+  echo "Already run today (${CURR_DATE}), skipping..."
+  exit 0
+fi
+
+apt update
+
+echo "${CURR_DATE}" > "${LAST_UPDATE_FILE}"
+
