9af8616fc7621ed6eef479ec663353acd6bdba2b
Tony Aguilar <onixag@gmail.com>
2023-08-03 21:08:24 +0200

    Remove slack ;-)

--- setup/scripts/src/debian-post-install/.specific-software.d/802-slack.sh
+++ setup/scripts/src/debian-post-install/.specific-software.d/802-slack.sh
@@ -1,15 +1,18 @@
-log_highlight -
-log_highlight - [${APP_NAME}] Installing slack
-log_highlight -
+#
+# DISABLED - Not needed anymore ;-)
+#
+# log_highlight -
+# log_highlight - [${APP_NAME}] Installing slack
+# log_highlight -
 
-VERSION="4.29.149"
-PLATFORM="amd64"
+# VERSION="4.29.149"
+# PLATFORM="amd64"
 
-SOURCE_PATH="https://downloads.slack-edge.com/linux_releases"
-SOURCE_FILE="slack-desktop-${VERSION}-${PLATFORM}.deb"
+# SOURCE_PATH="https://downloads.slack-edge.com/linux_releases"
+# SOURCE_FILE="slack-desktop-${VERSION}-${PLATFORM}.deb"
 
-TEMP_FOLDER=$(mktemp -d -t ${APP_NAME}-slack-XXXXXX)
+# TEMP_FOLDER=$(mktemp -d -t ${APP_NAME}-slack-XXXXXX)
 
-RUNCMD "wget --no-verbose -O ${TEMP_FOLDER}/${SOURCE_FILE} ${SOURCE_PATH}/${SOURCE_FILE}"
-RUNCMD "sudo apt-get -y install ${TEMP_FOLDER}/${SOURCE_FILE}"
-RUNCMD "rm -rf ${TEMP_FOLDER}"
+# RUNCMD "wget --no-verbose -O ${TEMP_FOLDER}/${SOURCE_FILE} ${SOURCE_PATH}/${SOURCE_FILE}"
+# RUNCMD "sudo apt-get -y install ${TEMP_FOLDER}/${SOURCE_FILE}"
+# RUNCMD "rm -rf ${TEMP_FOLDER}"
