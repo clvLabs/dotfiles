26b4e71bae9bbff2d95c0687737fc42bb43330ab
Tony Aguilar <onixag@gmail.com>
2023-11-30 02:37:46 +0100

    fif: add color/line numbers

--- scripts/fif.sh
+++ scripts/fif.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 grep \
- --color=auto \
+ --color=always \
  --binary-files=without-match \
  --recursive \
  --ignore-case \
  --perl-regexp \
+ --line-number \
  --include "*${1}" \
  "${2}" \
  ${3} ${4} ${5} ${6} ${7} ${8} ${9}
