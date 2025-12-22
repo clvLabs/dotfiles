724167b369c6c2dc0310a5c288e4fb21be42f70a
Tony Aguilar <onixag@gmail.com>
2025-12-22 10:09:30 +0100

    Install software-properties-common at start to have apt-add-repository available

--- setup/scripts/src/debian-post-install/01-update-os.sh
+++ setup/scripts/src/debian-post-install/01-update-os.sh
@@ -1,5 +1,11 @@
 log_highlight - ----------------------------------------------------------------------------
 log_highlight -
+log_highlight - [${APP_NAME}] Installing software-properties-common to have apt-add-repository available
+log_highlight -
+RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt-get -y install software-properties-common"
+
+log_highlight - ----------------------------------------------------------------------------
+log_highlight -
 log_highlight - [${APP_NAME}] Enabling "contrib" and "non-free" package repositories
 log_highlight -
 RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt-add-repository contrib"
