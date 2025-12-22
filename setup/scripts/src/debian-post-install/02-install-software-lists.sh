1cdccb39b6ab7d2f01ac289be36c836c6a296ba4
Tony Aguilar <onixag@gmail.com>
2025-12-22 12:44:41 +0100

    Install packages one by one (better logs)

--- setup/scripts/src/debian-post-install/02-install-software-lists.sh
+++ setup/scripts/src/debian-post-install/02-install-software-lists.sh
@@ -2,5 +2,9 @@ log_highlight - ----------------------------------------------------------------
 log_highlight -
 log_highlight - [${APP_NAME}] Installing package list for: apt
 log_highlight -
+# APT_INSTALL=$(echo $(sed "s/#.*$//g" ${SETUP_RESOURCES_DIR}/requirements/apt-install.txt | cat))
+# RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt-get -y install ${APT_INSTALL}"
 APT_INSTALL=$(echo $(sed "s/#.*$//g" ${SETUP_RESOURCES_DIR}/requirements/apt-install.txt | cat))
-RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt-get -y install ${APT_INSTALL}"
+for PKG in ${APT_INSTALL}; do
+  RUNCMD "DEBIAN_FRONTEND=noninteractive sudo apt-get -y install ${PKG}"
+done
