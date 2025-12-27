5452d2cc9e5926f40313ecae8ae0f8843961d0ab
Tony Aguilar <onixag@gmail.com>
2025-12-27 13:06:44 +0100

    Enhance aks-pc-rmt

--- scripts/aks-pc-rmt.sh
+++ scripts/aks-pc-rmt.sh
@@ -1,17 +1,23 @@
 #!/bin/zsh
 HOST=AKS_TAGUILAR.local
-# USER="Tony Aguilar"
 USER="taguilar"
 
-if [[ -z "${1}" ]]; then
+if ! [[ -z "${1}" ]]; then
   HOST=s-reches.local
-  # USER="taguilar"
 fi
 
-xfreerdp \
+PASS=$(yad --text "Password for $HOST" --entry --hide-text)
+
+if [[ -z "${PASS}" ]]; then
+  exit 0
+fi
+
+xfreerdp3 \
  /v:${HOST} \
  /cert:ignore \
+ /d:"" \
  /u:"${USER}" \
+ /p:"${PASS}" \
  /dynamic-resolution \
  -grab-keyboard \
  +clipboard
